6ff8f021793e583349789b4313b3dae49d6da7d6
Frédérique Laguerre <laguerre.frederique@gmail.com>
2023-11-29 15:39:59 +0100

    create bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,19 @@
 class BookingsController < ApplicationController
   def create
-    raise
+    @shark = Shark.find(params[:booking][:shark_id])
+    @booking = Booking.new(booking_params)
+    @booking.shark = @shark
+    @booking.renter = current_user
+    if @booking.save
+      redirect_to owner_dashboards_path
+    else
+      render "sharks/show", status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:start_date, :end_date, :shark_id)
   end
 end
