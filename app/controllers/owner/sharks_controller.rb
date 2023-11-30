fbcfca2035c4e9007abca2008089853a86ddc5ca
qianwang <wangqianlf@live.com>
2023-11-30 14:45:05 +0100

    1.formulaire create shark, 2.redirect à dashboard : sont faits

--- app/controllers/owner/sharks_controller.rb
+++ app/controllers/owner/sharks_controller.rb
@@ -1,7 +1,22 @@
 class Owner::SharksController < ApplicationController
   def new
+    @shark = Shark.new
   end
 
   def create
+    @shark = Shark.new(shark_params)
+    @shark.owner = current_user
+
+    if @shark.save
+      redirect_to owner_dashboards_path
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def shark_params
+    params.require(:shark).permit(:name, :description, :price_per_day, :address, :weight, :skills, photos: [])
   end
 end
